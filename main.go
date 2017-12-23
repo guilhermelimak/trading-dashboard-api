7555ddb8db55d56e8f030e87d80e08e95d2401ee
Guilherme Lima <guilhermelimak@gmail.com>
2017-12-23 17:50:07 -0200

    refactor: Add variables for addresses and ports

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
+// Port the server will listen on.
+const Port = 8888
+
+// Address the server will listen on.
+const Address = "0.0.0.0"
+
+// ServerAddress formatted on address:port format.
+var ServerAddress = fmt.Sprintf("%s:%d", Address, Port)
+
+// ClientAddress to be allowed on cors.
+const ClientAddress = "http://localhost:8080"
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/mercadobtc/{coin}/{method}", mercadoHandler)
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowedOrigins:   []string{ClientAddress},
 		AllowCredentials: true,
 	})
 
 	http.Handle("/", c.Handler(router))
 
-	err := http.ListenAndServe(":8888", nil)
+	fmt.Printf("Listening on: %s...", ServerAddress)
+
+	err := http.ListenAndServe(ServerAddress, nil)
 
 	if err != nil {
 		panic(err)
